3d495d901c001366f44ab583968619933220c558
Sahil Chouhan <chouhangolu604@gmail.com>
2026-02-04 16:11:37 +0530

    new file

---
+++ chatgpt_codes/day3.js
@@ -0,0 +1,14 @@
+//array in typeScript
+var mySkills = ["HTML", "CSS", "Js"];
+console.log(mySkills);
+var updatedSkills = mySkills.push("Typescript");
+console.log(mySkills);
+var movies = ["KGF", "Pushpa", "3 Idiots"];
+movies.push("Dhurandhar");
+console.log(movies);
+var data = ["sahil", 1];
+console.log(data);
+var number = [1, 2, 4, 5];
+console.log(number);
+var flags = [true, "maya"];
+console.log(flags);
---
+++ chatgpt_codes/day3.ts
@@ -0,0 +1,19 @@
+//array in typeScript
+let mySkills:string[] = ["HTML", "CSS", "Js"]
+console.log(mySkills)
+let updatedSkills = mySkills.push("Typescript")
+console.log(mySkills)
+
+let movies: string[] = ["KGF", "Pushpa", "3 Idiots"]
+movies.push("Dhurandhar")
+console.log(movies)
+
+let data:(number|string)[] = ["sahil", 1]
+console.log(data)
+
+let number:number[] = [1, 2, 4 ,5]
+console.log(number)
+
+let flags:(string|boolean)[]=[true, "maya"]
+
+console.log(flags)
