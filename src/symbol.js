e903e4280fba8db3c2dc14fe9f16b8fb074caa36
Sahil Chouhan <chouhangolu604@gmail.com>
2026-02-09 21:38:01 +0530

    update

---
+++ src/symbol.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var sym = Symbol();
+var sym2 = Symbol();
+console.log(sym === sym2);
---
+++ symbol.ts
@@ -0,0 +1,4 @@
+var sym = Symbol()
+var sym2 = Symbol()
+
+console.log(sym===sym2)
