b3c5fa97530c2243841e4ff2b32e7b7200300332
Sahil Chouhan <chouhangolu604@gmail.com>
2026-02-11 22:11:28 +0530

    new file

---
+++ array.ts
@@ -0,0 +1,10 @@
+var users: string[] = ["anil", "sam", "peter"]
+console.log(users);
+
+
+users.push("mhit")
+console.log(users);
+
+
+var collegeName: readonly number[] = [1, 2, 4, 5]
+console.log(collegeName);
---
+++ src/array.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var users = ["anil", "sam", "peter"];
+console.log(users);
+users.push("mhit");
+console.log(users);
+var collegeName = [1, 2, 4, 5];
+console.log(collegeName);
