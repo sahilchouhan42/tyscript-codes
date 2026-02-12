5627d5ea1088327d2f01affe69af2a9110e08ca1
Sahil Chouhan <chouhangolu604@gmail.com>
2026-02-12 22:07:06 +0530

    new file

---
+++ src/tuple.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var data = ["sahil", 45, true];
+console.log(data);
+data.push("indore");
+console.log(data);
---
+++ tuple.ts
@@ -0,0 +1,6 @@
+var data:[string, number, boolean] = ["sahil", 45, true]
+
+console.log(data);
+data.push("indore")
+console.log(data);
+
